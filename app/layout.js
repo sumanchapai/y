ab2f8da72940c80da2bc0c1abe6145a46a4045f2
suman chapai <sumanchapai@gmail.com>
2022-11-04 21:00:36 -0500

    Add comments

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
 async function testdata() {
+    // Surprisingly, there's no error if we dont specify the options praram
     const res = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
         next: { revalidate: 10 },
     });
     return res.json();
 }
-
 
 export default async function RootLayout({ children }) {
     const data = await testdata();
--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ export default function Main() {
     return (
         <div>
             <h1>Hello, world!</h1>
+            {/* Very surprisingly, there is no errro if you remove this Link */}
+            {/* Try clicking on this link in the homepage, and refresh your browser */}
             <Link href="/sell">Sell</Link>
         </div>
     );
